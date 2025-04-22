e60a09388356263ee7e5c69186ce3e0bfbf163ad
lopo <lopo@zju.edu.cn>
2025-04-22 19:51:24 +0800

    feat: delegate

--- lib/bad_markdown.dart
+++ lib/bad_markdown.dart
@@ -1,7 +1,64 @@
 library bad_markdown;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+part 'renderer.dart';
+
+part 'style.dart';
+
+part 'token.dart';
+
+class BadMarkdown extends StatefulWidget {
+  final String content;
+
+  const BadMarkdown({super.key, required this.content});
+
+  @override
+  State<BadMarkdown> createState() => _MarkdownState();
 }
+
+class _MarkdownState extends State<BadMarkdown> {
+  List<MarkdownToken> tokens = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    tokens = MarkdownToken.parse(widget.content);
+  }
+
+  @override
+  void didUpdateWidget(covariant BadMarkdown oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.content != oldWidget.content) {
+      tokens = MarkdownToken.parse(widget.content);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [for (final token in tokens) token.render()],
+      ),
+      style: BadMarkdownStyles.base,
+    );
+  }
+}
+
+// class _MarkdownRendererDelegate extends StatelessWidget {
+//   final List<MarkdownToken> tokens;
+//
+//   const _MarkdownRendererDelegate(this.tokens);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text.rich(
+//       TextSpan(
+//         children: [for (final token in tokens) token.render()],
+//       ),
+//       style: BadMarkdownStyles.base,
+//     );
+//   }
+// }
